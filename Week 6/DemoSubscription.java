5b156d5ece0c5d6c02035cbdc0f0c5871899afdd
Hendry <hendry.kosasih@yahoo.com>
2016-12-27 13:21:14 +0800

    Commits

--- Week 6/DemoSubscription.java
+++ Week 6/DemoSubscription.java
@@ -2,7 +2,7 @@ public class DemoSubscription {
 
     public static void main(String[] args)
     {
-        NewspaperSubscription man1 = new PhysicalNewspaperSubscription("Local","Turk",10);
+        NewspaperSubscription man1 = new PhysicalNewspaperSubscription("Local","10",10);
         NewspaperSubscription man2 = new PhysicalNewspaperSubscription("International","Ball",10);
         NewspaperSubscription man3 = new OnlineNewspaperSubscription("National","Lost",30);
         NewspaperSubscription man4 = new OnlineNewspaperSubscription("Region","Angel",30);
